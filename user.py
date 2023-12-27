269891abca40e7f2dd308c184abd451656b47798
Gaurika Dwivedi <72243047+GaurikaDwivedi@users.noreply.github.com>
2023-12-27 19:38:45 +0530

    Delete user.py

--- user.py
+++
@@ -1,9 +0,0 @@
-class User:
-    def __init__(self, name):
-        self.name = name
-    
-    def updateName(self, newName):
-        self.name = newName
-    
-    def getName(self):
-        return self.name
